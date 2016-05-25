769b30d922e686923b50021d8b91e6338819391e
RudolphL <rudolphlesire@gmail.com>
2016-05-25 10:19:25 +0200

    Service template added.

---
+++ ProEpService/ProEpService/ILogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProEpService
+{
+    [ServiceContract]
+    public interface ILogin
+    {
+        [OperationContract]
+        int Login(string playername, string password);
+
+        [OperationContract]
+        String RecoverPassword(string email);
+
+        [OperationContract]
+        int CreateAccount(string firstname, string lastname, string playername, string email, string password);
+    }
+}
---
+++ ProEpService/ProEpService/ServiceApplication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProEpService
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Reentrant)]
+    public class ServiceApplication : ILogin
+    {
+        public int Login(string playername, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RecoverPassword(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CreateAccount(string firstname, string lastname, string playername, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
