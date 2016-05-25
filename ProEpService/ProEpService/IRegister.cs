7d23ba5b636fa6a598ee4b1ad67ef656113fb0b2
sacnuoc1102 <pookidloveu@gmail.com>
2016-05-25 11:06:31 +0200

    add IRegister and Post classes

---
+++ ProEpService/ProEpService/IRegister.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ProEpService
+{
+    [ServiceContract]
+    public interface IRegister
+    {
+        // use for register the code will use DatabaseHelper classes and then check and then regis
+        [OperationContract]
+        bool Register(string username, string password, string name, string city);
+
+
+    }
+}
---
+++ ProEpService/ProEpService/Post.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEpService
+{
+    public class Post
+    {
+        //fields
+        private String description;
+        private String title;
+
+        //properties
+        public String Description
+        {
+            get { return this.description; }
+            private set { this.description = value; }
+        }
+
+
+        //constructor
+
+        //methods
+
+
+    }
+}
