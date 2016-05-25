ba03ea6f82e23cd0c462239d28676782534f882a
Yiyu Xu <xuyiyu1992@gmail.com>
2016-05-25 10:20:50 +0200

    connect the form

--- ProEpBookApp/ProEpBookApp/Form1.Designer.cs
+++ ProEpBookApp/ProEpBookApp/Form1.Designer.cs
@@ -38,25 +38,25 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.btnFilterAuthor = new System.Windows.Forms.Button();
-            this.btnFilterISBN = new System.Windows.Forms.Button();
-            this.btnFilterPrice = new System.Windows.Forms.Button();
-            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txbSearch = new System.Windows.Forms.TextBox();
             this.listView1 = new System.Windows.Forms.ListView();
             this.Author = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Description = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Price = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Place = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnLogin = new System.Windows.Forms.Button();
-            this.btnRegister = new System.Windows.Forms.Button();
-            this.txbSearch = new System.Windows.Forms.TextBox();
-            this.btnSearch = new System.Windows.Forms.Button();
-            this.txbFilterAuthor = new System.Windows.Forms.TextBox();
-            this.txbFilterISBN = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.txbFilterPrice = new System.Windows.Forms.TextBox();
+            this.txbFilterISBN = new System.Windows.Forms.TextBox();
+            this.txbFilterAuthor = new System.Windows.Forms.TextBox();
+            this.btnFilterPrice = new System.Windows.Forms.Button();
+            this.btnFilterISBN = new System.Windows.Forms.Button();
+            this.btnFilterAuthor = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -99,6 +99,7 @@
             this.btnMyMessage.TabIndex = 3;
             this.btnMyMessage.Text = "My Message";
             this.btnMyMessage.UseVisualStyleBackColor = true;
+            this.btnMyMessage.Click += new System.EventHandler(this.btnMyMessage_Click);
             // 
             // btnAbout
             // 
@@ -154,6 +155,35 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "User";
             // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(237, 12);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(73, 41);
+            this.btnRegister.TabIndex = 2;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(158, 12);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(73, 41);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name";
+            // 
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.btnSearch);
@@ -166,53 +196,22 @@
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Posts";
             // 
-            // groupBox5
+            // btnSearch
             // 
-            this.groupBox5.Controls.Add(this.txbFilterPrice);
-            this.groupBox5.Controls.Add(this.txbFilterISBN);
-            this.groupBox5.Controls.Add(this.txbFilterAuthor);
-            this.groupBox5.Controls.Add(this.btnFilterPrice);
-            this.groupBox5.Controls.Add(this.btnFilterISBN);
-            this.groupBox5.Controls.Add(this.btnFilterAuthor);
-            this.groupBox5.Location = new System.Drawing.Point(801, 255);
-            this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(273, 263);
-            this.groupBox5.TabIndex = 10;
-            this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "Filter";
+            this.btnSearch.Location = new System.Drawing.Point(331, 37);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(69, 30);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
-            // btnFilterAuthor
+            // txbSearch
             // 
-            this.btnFilterAuthor.Location = new System.Drawing.Point(30, 50);
-            this.btnFilterAuthor.Name = "btnFilterAuthor";
-            this.btnFilterAuthor.Size = new System.Drawing.Size(225, 38);
-            this.btnFilterAuthor.TabIndex = 0;
-            this.btnFilterAuthor.Text = "Author Search";
-            this.btnFilterAuthor.UseVisualStyleBackColor = true;
-            // 
-            // btnFilterISBN
-            // 
-            this.btnFilterISBN.Location = new System.Drawing.Point(30, 129);
-            this.btnFilterISBN.Name = "btnFilterISBN";
-            this.btnFilterISBN.Size = new System.Drawing.Size(225, 38);
-            this.btnFilterISBN.TabIndex = 2;
-            this.btnFilterISBN.Text = "ISBN Search";
-            this.btnFilterISBN.UseVisualStyleBackColor = true;
-            // 
-            // btnFilterPrice
-            // 
-            this.btnFilterPrice.Location = new System.Drawing.Point(30, 205);
-            this.btnFilterPrice.Name = "btnFilterPrice";
-            this.btnFilterPrice.Size = new System.Drawing.Size(225, 38);
-            this.btnFilterPrice.TabIndex = 4;
-            this.btnFilterPrice.Text = "Price Search";
-            this.btnFilterPrice.UseVisualStyleBackColor = true;
-            // 
-            // imageList1
-            // 
-            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
-            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
-            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.txbSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSearch.Location = new System.Drawing.Point(34, 38);
+            this.txbSearch.Name = "txbSearch";
+            this.txbSearch.Size = new System.Drawing.Size(291, 29);
+            this.txbSearch.TabIndex = 1;
             // 
             // listView1
             // 
@@ -221,7 +220,7 @@
             this.Description,
             this.Price,
             this.Place});
-            this.listView1.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listView1.GridLines = true;
             this.listView1.Location = new System.Drawing.Point(34, 81);
             this.listView1.Name = "listView1";
@@ -250,57 +249,28 @@
             this.Place.Text = "Place";
             this.Place.Width = 390;
             // 
-            // label1
+            // groupBox5
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "User Name";
+            this.groupBox5.Controls.Add(this.txbFilterPrice);
+            this.groupBox5.Controls.Add(this.txbFilterISBN);
+            this.groupBox5.Controls.Add(this.txbFilterAuthor);
+            this.groupBox5.Controls.Add(this.btnFilterPrice);
+            this.groupBox5.Controls.Add(this.btnFilterISBN);
+            this.groupBox5.Controls.Add(this.btnFilterAuthor);
+            this.groupBox5.Location = new System.Drawing.Point(801, 255);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(273, 263);
+            this.groupBox5.TabIndex = 10;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Filter";
             // 
-            // btnLogin
+            // txbFilterPrice
             // 
-            this.btnLogin.Location = new System.Drawing.Point(158, 12);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(73, 41);
-            this.btnLogin.TabIndex = 1;
-            this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = true;
-            // 
-            // btnRegister
-            // 
-            this.btnRegister.Location = new System.Drawing.Point(237, 12);
-            this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(73, 41);
-            this.btnRegister.TabIndex = 2;
-            this.btnRegister.Text = "Register";
-            this.btnRegister.UseVisualStyleBackColor = true;
-            // 
-            // txbSearch
-            // 
-            this.txbSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbSearch.Location = new System.Drawing.Point(34, 38);
-            this.txbSearch.Name = "txbSearch";
-            this.txbSearch.Size = new System.Drawing.Size(291, 29);
-            this.txbSearch.TabIndex = 1;
-            // 
-            // btnSearch
-            // 
-            this.btnSearch.Location = new System.Drawing.Point(331, 37);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(69, 30);
-            this.btnSearch.TabIndex = 2;
-            this.btnSearch.Text = "Search";
-            this.btnSearch.UseVisualStyleBackColor = true;
-            // 
-            // txbFilterAuthor
-            // 
-            this.txbFilterAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbFilterAuthor.Location = new System.Drawing.Point(30, 19);
-            this.txbFilterAuthor.Name = "txbFilterAuthor";
-            this.txbFilterAuthor.Size = new System.Drawing.Size(225, 29);
-            this.txbFilterAuthor.TabIndex = 3;
+            this.txbFilterPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbFilterPrice.Location = new System.Drawing.Point(30, 173);
+            this.txbFilterPrice.Name = "txbFilterPrice";
+            this.txbFilterPrice.Size = new System.Drawing.Size(225, 29);
+            this.txbFilterPrice.TabIndex = 6;
             // 
             // txbFilterISBN
             // 
@@ -310,13 +280,46 @@
             this.txbFilterISBN.Size = new System.Drawing.Size(225, 29);
             this.txbFilterISBN.TabIndex = 5;
             // 
-            // txbFilterPrice
+            // txbFilterAuthor
             // 
-            this.txbFilterPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbFilterPrice.Location = new System.Drawing.Point(30, 173);
-            this.txbFilterPrice.Name = "txbFilterPrice";
-            this.txbFilterPrice.Size = new System.Drawing.Size(225, 29);
-            this.txbFilterPrice.TabIndex = 6;
+            this.txbFilterAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbFilterAuthor.Location = new System.Drawing.Point(30, 19);
+            this.txbFilterAuthor.Name = "txbFilterAuthor";
+            this.txbFilterAuthor.Size = new System.Drawing.Size(225, 29);
+            this.txbFilterAuthor.TabIndex = 3;
+            // 
+            // btnFilterPrice
+            // 
+            this.btnFilterPrice.Location = new System.Drawing.Point(30, 205);
+            this.btnFilterPrice.Name = "btnFilterPrice";
+            this.btnFilterPrice.Size = new System.Drawing.Size(225, 38);
+            this.btnFilterPrice.TabIndex = 4;
+            this.btnFilterPrice.Text = "Price Search";
+            this.btnFilterPrice.UseVisualStyleBackColor = true;
+            // 
+            // btnFilterISBN
+            // 
+            this.btnFilterISBN.Location = new System.Drawing.Point(30, 129);
+            this.btnFilterISBN.Name = "btnFilterISBN";
+            this.btnFilterISBN.Size = new System.Drawing.Size(225, 38);
+            this.btnFilterISBN.TabIndex = 2;
+            this.btnFilterISBN.Text = "ISBN Search";
+            this.btnFilterISBN.UseVisualStyleBackColor = true;
+            // 
+            // btnFilterAuthor
+            // 
+            this.btnFilterAuthor.Location = new System.Drawing.Point(30, 50);
+            this.btnFilterAuthor.Name = "btnFilterAuthor";
+            this.btnFilterAuthor.Size = new System.Drawing.Size(225, 38);
+            this.btnFilterAuthor.TabIndex = 0;
+            this.btnFilterAuthor.Text = "Author Search";
+            this.btnFilterAuthor.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
             // Form1
             // 
--- ProEpBookApp/ProEpBookApp/Form1.cs
+++ ProEpBookApp/ProEpBookApp/Form1.cs
@@ -12,9 +12,36 @@ namespace ProEpBookApp
 {
     public partial class Form1 : Form
     {
+        LoginForm lForm = null;
+        Form1 mainForm = null;
+        Register rForm = null;
+        MessageForm mForm = null;
+
         public Form1()
         {
             InitializeComponent();
+            mainForm = new Form1();
+            lForm = new LoginForm();
+            rForm = new Register();
+            mForm = new MessageForm();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            lForm.Show();
+            mainForm.Hide();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            rForm.Show();
+            mainForm.Hide();
+        }
+
+        private void btnMyMessage_Click(object sender, EventArgs e)
+        {
+            mForm.Show();
+            mainForm.Hide();
         }
     }
 }
