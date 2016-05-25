7db47a91a7da355663c5f9d46298a5cdbdcf2ba6
sacnuoc1102 <pookidloveu@gmail.com>
2016-05-25 23:46:09 +0200

    implement Message class and IMessage interface

--- ProEpService/ProEpService/IMessage.cs
+++ ProEpService/ProEpService/IMessage.cs
@@ -6,14 +6,54 @@ using System.Threading.Tasks;
 using System.ServiceModel;
 namespace ProEpService
 {
+
+
     [ServiceContract(Namespace = "ProEpService", CallbackContract = typeof(IUpdateMessageCallback))]
     public interface IMessage
     {        
+
+        /// <summary>
+        /// This method will unsubscribe the client from getting notify by the callback of IUpdateMessage
+        /// </summary>
         [OperationContract]
         void UpdateMessageUnsubcribe();
 
+        /// <summary>
+        /// This method will subcribe the client to receiver a trigger from the client( a call back) with IUpdateMessage 
+        /// </summary>
         [OperationContract]
         void UpdateMessageSubscribe();
+
+        /// <summary>
+        /// This method will send the message text to the database and then notify the client (refresh)
+        /// The message will be send as a Message type object which contain : sender, receiver, id and text.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        [OperationContract]
+        bool SendMessage(Message message);
+
+        /// <summary>
+        /// This method will get the ListOfPerson that the user have chated with before.
+        /// </summary>
+        /// <param name="user"> the username of the current user</param>
+        /// <returns></returns>
+        [OperationContract]
+        List<String> GetListOfPerson(String username);
+
+        /// <summary>
+        /// This method will get the ListOfMessage( return a List of type Message) 
+        /// by searching through the database and then find the message 
+        /// with sender and user like parameters had declared
+        /// </summary>
+        /// <param name="sender_username">user name of sender</param>
+        /// <param name="receiver_username">user name of receiver</param>
+        /// <returns></returns>
+        [OperationContract]
+        List<Message> GetListOfMessage(String sender_username, String receiver_username);
+
+      
+
     }
 
 
---
+++ ProEpService/ProEpService/Message.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProEpService
+{
+    public class Message
+    {
+
+        //fields
+        private int message_id;                //id possition of the message 
+        private String messagetext;            //the message (text)
+        private String client_sender;          //the name of the one who send the text
+        private String client_receiver;        //the name of the one who receive the text
+
+        //properties
+        public int Message_id
+        {
+            get { return this.message_id; }
+            private set { this.message_id = value; }
+        }
+
+        public String MessageText
+        {
+            get { return this.messagetext; }
+            private  set { this.messagetext = value; }
+        }
+
+        public String Client_sender
+        {
+            get { return this.client_sender; }
+            private set { this.client_sender = value; }
+        }
+
+        public String Client_receiver
+        {
+            get { return this.client_receiver; }
+            private set { this.client_receiver = value; }
+        }
+
+        //constructor
+
+        public Message(int message_id, String messagetext, String client_sender, String client_receiver)
+        {
+            this.Message_id = message_id;
+            this.MessageText = messagetext;
+            this.Client_sender = client_sender;
+            this.Client_receiver = client_receiver;
+
+        }
+
+        //method
+
+        
+    }
+}
