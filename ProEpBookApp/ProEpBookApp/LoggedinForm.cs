8f7f32a49ad88d0b07e6dbcc1660a94ce537d322
RudolphL <rudolphlesire@gmail.com>
2016-06-08 09:38:31 +0200

    Removed the created forms in the main application. They crash the application.

--- ProEpBookApp/ProEpBookApp/LoggedinForm.cs
+++ ProEpBookApp/ProEpBookApp/LoggedinForm.cs
@@ -12,41 +12,25 @@ namespace ProEpBookApp
 {
     public partial class LoggedinForm : Form
     {
-        LoginForm lForm = null;
-        LoggedinForm mainForm = null;
-        RegisterForm rForm = null;
-        MessageForm mForm = null;
-
         public LoggedinForm()
         {
             InitializeComponent();
-            mainForm = new LoggedinForm();
-            lForm = new LoginForm();
-            rForm = new RegisterForm();
-            mForm = new MessageForm();
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            lForm.Show();
-            mainForm.Hide();
         }
 
         private void btnRegister_Click(object sender, EventArgs e)
         {
-            rForm.Show();
-            mainForm.Hide();
         }
 
         private void btnMyMessage_Click(object sender, EventArgs e)
         {
-            mForm.Show();
-            mainForm.Hide();
         }
 
         private void btnCreateAPost_Click(object sender, EventArgs e)
         {
-
         }
     }
 }
